1a81f7168e03463dbf9971be18fcc151766ca80a
Nathaniel Geiger <Nathaniel-Geiger@live.nmit.ac.nz>
2024-03-07 16:26:23 +1300

    added if else

--- src/numbers.js
+++ src/numbers.js
@@ -1,9 +1,43 @@
-//NOTE - const is a permanet value while let and var can be changed
-const num1 = 10 
-console.log(num1)
+// //NOTE - const is a permanet value while let and var can be changed
+// /*
 
-let num2 = 20
-console.log(num2)
+// */
+// const pi = 3.14159
+// //pi = 10
+// console.log(pi)
 
-var num3 = 30
-console.log(num3)
+// let num2 = 20
+// //num2 = 25
+// console.log(num2)
+
+// var num3 = 30
+// console.log(num3)
+
+// const fullName = 'Nathaniel Geiger'
+// console.log(fullName)
+
+// const school = "NMIT"
+// console.log(school)
+
+// console.log(fullName + " " + school)
+
+// console.log(`My full name is ${fullName} and I am a student at ${school}.`)
+// const working = false
+// const notWorking = true
+
+// if(pi == 3.14159) {
+//     console.log(fullName)
+// }
+
+let a = 8
+let result
+if (a > 0) {
+    result = 'positive'
+    console.log(result)
+} else if (a < 0) {
+    result = 'not positive'
+    console.log(result)
+} else {
+    result = 'Zero'
+    console.log(result)
+}
