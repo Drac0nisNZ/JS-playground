dbeb40d2fbc1a2a0173572c75d5381a3f6686408
Nathaniel Geiger <Nathaniel-Geiger@live.nmit.ac.nz>
2024-03-14 14:08:51 +1300

    rock paper scissors 3 ways

--- src/function.js
+++ src/function.js
@@ -46,17 +46,62 @@
 
 
 
-//SECTION - write a function that takes an array with numbers and multiplies by 2 then returns new total
-// arrayByTwo([2,5,4])
-function arrayByTwo(arr) {
-    for (i = 0; i < arr.length; i++) {
-       arr[i] *= 2
-}
-return arr
-}
-console.log(arrayByTwo([2,5,4]))
+// //SECTION - write a function that takes an array with numbers and multiplies by 2 then returns new total
+// // arrayByTwo([2,5,4])
+// function arrayByTwo(arr) {
+//     for (i = 0; i < arr.length; i++) {
+//        arr[i] *= 2
+// }
+// return arr
+// }
+// console.log(arrayByTwo([2,5,4]))
 
-//One line
-const oneLineArray = (arr) => arr.map(el => el*2)
 
-console.log(oneLineArray([2,5,4]))
+
+
+// //One line
+// const oneLineArray = (arr) => arr.map(el => el*2)
+
+// console.log(oneLineArray([2,5,4]))
+
+//Create a function which takes two strings (p1 and p2) as arguments and returns a string
+//stating the winner (Rock, Paper, Scissors)
+
+//NOTE - Game Condition
+// if p1 wins return the string "The winner is P1"
+// if p2 wins return the string "The winner is P2"
+// if they are the same then return "It's a draw"
+
+// function rps(p1, p2) {
+//     if(p1 === p2) return `It's a draw`
+//     else if(p1 === 'Rock' && p2 === 'Scissors') return 'The winner is P1';
+//     else if(p1 === 'Paper' && p2 === 'Rock') return 'The winner is P1';
+//     else if(p1 === 'Scissors' && p2 === 'Paper') return 'The winner is P1';
+
+//     else return 'The winner is P2'
+// }
+
+// console.log(rps('Rock', 'Scissors'))
+
+// //ANCHOR - ES6 Javascript
+// function rpsTwo(p1, p2) {
+
+//     let obj = {
+//         Rock: 'Scissors',
+//         Scissors: 'Paper',
+//         Paper: 'Rock'
+//     }
+//     return p1 === p2 ? "It's a draw" : obj[p1] === p2 ? "The winner is P1" : "The winner is P2"
+// }
+
+// console.log(rpsTwo('Paper', 'Paper'))
+
+//ANCHOR - JS built in function includes()
+function rpsThree(p1, p2) {
+    const wins = ['RockScissors', 'PaperRock', 'ScissorsPaper']
+/* includes() searches the target for any matches with the assigned search and will give a return based on how the return 
+is written and if it is searching an array or a string. in this case it searches the input of p1 and p2 and compares them to
+the set win conditions*/ 
+    return p1 === p2 ? "it's a draw" : `The winner is ${wins.includes(p1 + p2) ? 'P1' : 'P2'}`
+}
+console.log(rpsThree('Paper', 'Scissors'))
